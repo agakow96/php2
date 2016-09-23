d70a1aed53bd65b7dca1c78d95e3e6131dc8ac0f
Agata Kowalczyk <agatakowalczyk1996@onet.pl>
2016-09-23 21:28:49 +0200

    Creating all subpages and adding styling it with css

---
+++ pets.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>About pets</title>
+<link href="../php/petsstyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+$curpage = 'pets.php';
+
+ require '../php/menu.php';?>
+<h1>About pets. </h1>
+
+<p>A pet or companion animal is an animal kept primarily for a person's company or protection, as opposed to working animals, sport animals, livestock, and laboratory animals, which are kept primarily for performance, agricultural value, or research. The most popular pets are noted for their attractive appearances and their loyal or playful personalities.
+
+Pets provide their owners (or guardians[1]) physical and emotional benefits. Walking a dog can supply both the human and pet with exercise, fresh air, and social interaction. Pets can give companionship to elderly adults who do not have adequate social interaction with other people, as well as other people that are living alone. There is a medically approved class of therapy animals, mostly dogs or cats, that are brought to visit confined humans. Pet therapy utilizes trained animals and handlers to achieve specific physical, social, cognitive, and emotional goals with patients.
+
+The most popular pets are likely dogs and cats, but people also keep house rabbits, ferrets; rodents such as gerbils, hamsters, chinchillas, fancy rats, and guinea pigs; avian pets, such as canaries, parakeets, corvids and parrots; reptile pets, such as turtles, lizards and snakes; aquatic pets, such as goldfish, tropical fish and frogs; and arthropod pets, such as tarantulas and hermit crabs.
+
+Some scholars and animal rights organizations have raised concern over pet-keeping with regards to the autonomy and objectification of nonhuman animals.</p>
+<img src="../php/line-pets1.png" width="640" height="297" alt=""/>
+<?php require '../php/footer.php';?>
+</body>
+</html>
---
+++ wildanimals.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Wild animals</title>
+<link href="../php/stylewildanimals.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+$curpage = 'wildanimals.php';
+
+ require '../php/menu.php';?>
+<h1>About wild animals</h1>
+
+<p>Wildlife traditionally refers to undomesticated animal species, but has come to include all plants, fungi, and other organisms that grow or live wild in an area without being introduced by humans.[1]
+<br>
+Wildlife can be found in all ecosystems. Deserts, forests, rain forests, plains, grasslands, and other areas including the most developed urban sites, all have distinct forms of wildlife. While the term in popular culture usually refers to animals that are untouched by human factors,[2] most scientists agree that much wildlife is affected by human activities.
+<br>
+Humans have historically tended to separate civilization from wildlife in a number of ways including the legal, social, and moral sense. Some animals, however, have adapted to suburban environments. This includes such animals as domesticated cats, dogs, mice, and gerbils. Some religions declare certain animals to be sacred, and in modern times concern for the natural environment has provoked activists to protest against the exploitation of wildlife for human benefit or entertainment.</p>
+<img src="../php/wildanimals.png" width="677" height="180" alt=""/>
+<?php require '../php/footer.php';?>
+</body>
+</html>
