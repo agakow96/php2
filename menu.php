255db174091d8ee27bd0c5e7f7b050aba1f26976
Agata Kowalczyk <agatakowalczyk1996@onet.pl>
2016-09-23 21:32:20 +0200

    Creating menu and footer that is used in all previous folders

---
+++ menu.php
@@ -0,0 +1,10 @@
+<link href="../php/menustyle.css" rel="stylesheet" type="text/css">
+<ul>
+<li><a href="../php/homepage.php" <?php if ($curpage == 'homepage.php') {echo 'class="active"'; }?>> HOME</a></li>
+<li><a href="../php/pets.php" <?php if ($curpage == 'pets.php') {echo 'class="active"'; }?>> PETS</a></li>
+<li><a href="../php/livestock.php" <?php if ($curpage == 'livestock.php') {echo 'class="active"'; }?>> LIVESTOCK</a></li>
+<li><a href="../php/wildanimals.php" <?php if ($curpage == 'wildanimals.php') {echo 'class="active"'; }?>> WILD ANIMALS</a></li>
+
+</ul>
+
+
