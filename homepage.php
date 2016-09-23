2152ebca28773ec7cf089a6457b3af6ac87cc49b
Agata Kowalczyk <agatakowalczyk1996@onet.pl>
2016-09-23 21:22:20 +0200

    Adding first page: home page
    I’ve created a short homepage for the website, here is a small
    introduction

---
+++ homepage.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home page</title>
+<link href="../php/stylehomage.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<?php
+$curpage = 'homepage.php';
+	
+
+ require '../php/menu.php';?>
+
+<h1>Hello!</h1>
+
+<p>Welcome at our website dedicated to different kind of animals. By reading this you will gain knowladdge about various spieces and diffent kind of animal. Hope you will enjoy reading this as much as we enjoy creating this for you! :)</p>
+<img src="../php/groupofanimals.png" width="525" height="360" alt=""/>
+<?php require '../php/footer.php';?>
+
+</body>
+</html>
